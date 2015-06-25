078eada0811bfa7aa9aee595c3c38daae6184862
vincent-courtalon <vincent.courtalon@gmail.com>
2015-06-25 16:46:27 +0200

    calcul nb de jours et bissextile

--- formation/DefenceCalendrier/src/principal/Program.java
+++ formation/DefenceCalendrier/src/principal/Program.java
@@ -16,8 +16,46 @@ public class Program {
 	}
 	
 	public static String calendrier(int annee, int mois) {
-		return header(annee, mois);
+		return header(annee, mois)
+				+ corps(annee, mois);
 	}
+	
+	public static String corps(int annee, int mois) {
+		StringBuilder sb = new StringBuilder();
+		int nbjours = nbJoursMois(annee, mois);
+		boolean leapyear = estBissextile(annee);
+		sb.append("nbjours = " + nbjours + " bissextile = " + leapyear);
+		return sb.toString();
+	}
+	
+	
+	public static int nbJoursMois(int annee, int mois) {
+		switch(mois) {
+			case 1:
+			case 3:
+			case 5:
+			case 7:
+			case 8:
+			case 10:
+			case 12:
+				return 31;
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+				return 30;
+			case 2:
+				return (estBissextile(annee)? 29 : 28);
+		}
+		return 0;
+	}
+	
+	
+	public static boolean estBissextile(int annee) {
+		return (annee % 4 == 0 && annee % 100 != 0 || annee % 400 == 0);
+	}
+	
+	
 	
 	public static String header(int annee, int mois) {
 		StringBuilder sb = new StringBuilder("----------------------------\n");
