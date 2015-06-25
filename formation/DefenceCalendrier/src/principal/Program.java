e7fa7553ba271a9f4272bc263e1c001d37ed6758
vincent-courtalon <vincent.courtalon@gmail.com>
2015-06-25 16:21:55 +0200

    affichage du header

--- formation/DefenceCalendrier/src/principal/Program.java
+++ formation/DefenceCalendrier/src/principal/Program.java
@@ -1,10 +1,45 @@
 package principal;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner reader = new Scanner(System.in);
+		System.out.println("annee ?");
+		int annee = Integer.parseInt(reader.nextLine());
+		System.out.println("mois");
+		int mois = Integer.parseInt(reader.nextLine());
+		
+		System.out.println(calendrier(annee, mois));
 
 	}
+	
+	public static String calendrier(int annee, int mois) {
+		return header(annee, mois);
+	}
+	
+	public static String header(int annee, int mois) {
+		StringBuilder sb = new StringBuilder("----------------------------\n");
+		sb.append("      ");
+		sb.append(nomMois(mois));
+		sb.append(" ");
+		sb.append(annee);
+		sb.append("\n----------------------------\n");
+		sb.append(  "LUN MAR MER JEU VEN SAM DIM\n");
+		return sb.toString();
+	}
+	
+	public static String nomMois(int mois) {
+		String[] tabmois = {"janvier", "fevrier", "mars", "avril", "mai",
+					"juin", "juillet", "aout", "septembre", "octobre",
+					"novembre", "decembre" };
+		
+		if (mois < 1 || mois > 12)
+			return "";
+		return tabmois[mois -1];
+	}
+	
+	
 
 }
