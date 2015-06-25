e8ad0bb33f804dca22d87d0d565e495fdec02a8d
vincent-courtalon <vincent.courtalon@gmail.com>
2015-06-25 11:43:13 +0200

    ajout fonction compteImpaire

--- formation/DefenceExerciceFonctions1/src/principal/Program.java
+++ formation/DefenceExerciceFonctions1/src/principal/Program.java
@@ -1,10 +1,29 @@
 package principal;
 
+import java.util.Arrays;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int[] entiers1 = {1,2,3,4,6,7,9};
+		int[] entiers2 = {101,20,31,14,11,27,59};
+		
+		System.out.println("nombre d'impaire dans " +
+					Arrays.toString(entiers1) + " = "
+					+ compteImpaire(entiers1));
+		System.out.println("nombre d'impaire dans " +
+				Arrays.toString(entiers2) + " = "
+				+ compteImpaire(entiers2));
+	}
+	
+	
+	public static int compteImpaire(int[] tab) {
+		int compteur = 0;
+		for (int i : tab) {
+			if (i % 2 != 0) {compteur++; }
+		}
+		return compteur;
 	}
 
 }
