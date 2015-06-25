3c41db54be31b8e5d1cfbae36e1f6cb6b892723d
vincent-courtalon <vincent.courtalon@gmail.com>
2015-06-25 12:05:53 +0200

    ajout compteAnnees et palindrome

--- formation/DefenceExerciceFonctions1/src/principal/Program.java
+++ formation/DefenceExerciceFonctions1/src/principal/Program.java
@@ -15,6 +15,47 @@ public class Program {
 		System.out.println("nombre d'impaire dans " +
 				Arrays.toString(entiers2) + " = "
 				+ compteImpaire(entiers2));
+		
+		System.out.println("'serres' est un palindrome ? " + estPalindrome("serres"));
+		System.out.println("'bonjour' est un palindrome ? " + estPalindrome("bonjour"));
+		System.out.println("'abcdcba' est un palindrome ? " + estPalindrome("abcdcba"));
+		System.out.println("'abedcba' est un palindrome ? " + estPalindrome("abedcba"));
+		
+		
+		System.out.println("somme = 1000.0, objectif = 2000.0, taux = 5% -> "
+				+ calculAnnee(1000.0, 2000.0, 0.05) + " années");
+		System.out.println("somme = 1000.0, objectif = 2000.0, taux = 3% -> "
+				+ calculAnnee(1000.0, 2000.0, 0.03) + " années");
+		
+	}
+	
+	public static int calculAnnee(double sommeInitiale,
+								  double sommeVoulue,
+								  double taux)
+	{
+		int annees = 0;
+		for ( ; sommeInitiale <= sommeVoulue; annees++) {
+			sommeInitiale += sommeInitiale * taux;
+		}
+	
+	/*	while (sommeInitiale <= sommeVoulue) {
+			sommeInitiale += sommeInitiale * taux;
+			annees++;
+		}*/
+		return annees;
+	}
+	
+	
+	public static boolean estPalindrome(String chaine) {
+		int start = 0;
+		int end = chaine.length() - 1;
+		while (start < end) {
+			if (chaine.charAt(start) != chaine.charAt(end))
+				return false;
+			start++;
+			end--;
+		}
+		return true;
 	}
 	
 	
