62b19283fd2c8453186ad43fd1eb115037bb42bc
vincent-courtalon <vincent.courtalon@gmail.com>
2015-07-17 15:30:00 +0200

    premier tag custom

---
+++ formationweb/webCustomTag/src/com/loncoto/webCustomTag/utils/HelloTag.java
@@ -0,0 +1,24 @@
+package com.loncoto.webCustomTag.utils;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+
+public class HelloTag extends SimpleTagSupport 
+{
+
+	@Override
+	public void doTag() throws JspException, IOException {
+		super.doTag();
+		PageContext pc = (PageContext)getJspContext();
+		JspWriter writer = pc.getOut();
+		writer.println("<h3>bonjour, nous sommes le " + new Date() + "</h3>");
+	}
+	
+	
+}
