eff792fe5c0e91ee2e7d23d218cb8c32002a9cba
vincent-courtalon <vincent.courtalon@gmail.com>
2015-07-17 11:45:34 +0200

    ajout requette orderby dans dao

--- formationweb/webArticle/WebContent/liste.jsp
+++ formationweb/webArticle/WebContent/liste.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -8,5 +9,15 @@
 </head>
 <body>
 <h2>liste des articles</h2>
+<table border="1">
+<tr><th>libelle</th><th>prix</th><th>actions</th></tr>
+<c:forEach items="${requestScope.articles}" var="a" >
+<tr>
+<td><c:out value="${a.libelle}" /></td>
+<td><c:out value="${a.prix}" /></td>
+<td>actions...</td>
+</tr>
+</c:forEach>
+</table>
 </body>
 </html>
--- formationweb/webArticle/src/com/loncoto/webArticle/utils/ArticleDAO.java
+++ formationweb/webArticle/src/com/loncoto/webArticle/utils/ArticleDAO.java
@@ -6,17 +6,28 @@ import java.util.*;
 import com.loncoto.webArticle.beans.Article;
 
 public class ArticleDAO {
+	
+	public static final int ORDERED_BY_DEFAULT = 0;
+	public static final int ORDERED_BY_PRIX = 1;
+	public static final int ORDERED_BY_POIDS = 2;
+	
 	public static final String FIND_ALL_SQL = "select * from `article`";
+	public static final String FIND_ALL_ORDERED_SQL = "select * from `article` ORDER BY ?";
+	
 	
 	private Connection base;
 	
 	private PreparedStatement findAllSatement;
+	private PreparedStatement findAllOrderedSatement;
+	
 	
 	public ArticleDAO(Connection base) {
 		this.base = base;
 		
 		try {
 			findAllSatement = base.prepareStatement(FIND_ALL_SQL);
+			findAllOrderedSatement = base.prepareStatement(FIND_ALL_ORDERED_SQL);
+					
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -30,12 +41,34 @@ public class ArticleDAO {
 		return null;
 	}
 	
-	public List<Article> findAll() {
+	
+	public List<Article> findAll() {return findAll(ORDERED_BY_DEFAULT); }
+	public List<Article> findAll(int orderType) {
 		ArrayList<Article> articles = new ArrayList<Article>();
 		
+		
 		try {
-			findAllSatement.clearParameters();
-			ResultSet rs = findAllSatement.executeQuery();
+			// stat contiendra la requette choisi pour execution
+			// soit la requette basique, soit une requette avec orderby
+			// et on choisi aussi la colonne pour orderby le cas échéant
+			PreparedStatement stat = findAllSatement;
+			switch (orderType) {
+				case ORDERED_BY_DEFAULT:
+					findAllSatement.clearParameters();
+					stat = findAllSatement;
+					break;
+				case ORDERED_BY_POIDS:
+					findAllOrderedSatement.clearParameters();
+					findAllOrderedSatement.setString(1, "poids");
+					stat = findAllOrderedSatement;
+					break;
+				case ORDERED_BY_PRIX:
+					findAllOrderedSatement.clearParameters();
+					findAllOrderedSatement.setString(1, "prix");
+					stat = findAllOrderedSatement;
+					break;
+			}
+			ResultSet rs = stat.executeQuery();
 			while (rs.next()) {
 				articles.add(fetchRow(rs));
 			}
