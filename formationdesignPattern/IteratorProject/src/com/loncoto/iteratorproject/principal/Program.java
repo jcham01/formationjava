66b9a85a76820f9ee4de623bfd8efa4a4d4724e0
vincent-courtalon <vincent.courtalon@gmail.com>
2015-07-20 14:43:39 +0200

    interval ip fonctionnel

---
+++ formationdesignPattern/IteratorProject/src/com/loncoto/iteratorproject/principal/Program.java
@@ -0,0 +1,18 @@
+package com.loncoto.iteratorproject.principal;
+
+import com.loncoto.iteratorproject.reseau.IPInterval;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		IPInterval ipi1 = new IPInterval(100, 100, 100, 50, 100, 100, 110, 25);
+		
+		System.out.println("100.100.100.40 dedans? " + ipi1.isInInterval(100, 100, 100, 40));
+		System.out.println("100.100.100.60 dedans? " + ipi1.isInInterval(100, 100, 100, 60));
+		System.out.println("100.100.105.10 dedans? " + ipi1.isInInterval(100, 100, 105, 10));
+		System.out.println("100.100.110.30 dedans? " + ipi1.isInInterval(100, 100, 110, 30));
+		
+	}
+
+}
