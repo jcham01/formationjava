b9e0b2cb29944c78bdd23fd5d2386cd7a212990c
vincent-courtalon <vincent.courtalon@gmail.com>
2015-07-21 14:41:23 +0200

    ajout des join

--- formationdesignPattern/TestThread1/src/com/loncoto/testthread1/principal/Program.java
+++ formationdesignPattern/TestThread1/src/com/loncoto/testthread1/principal/Program.java
@@ -12,15 +12,33 @@ public class Program {
 		
 		// le thread est crée, mais non démarré
 		Thread t1 = new Thread(w1);
-		Thread t2 = new Thread(w2);		
+		Thread t2 = new Thread(w2);
+		Thread t3 = new Thread(w3);
 		
 		System.out.println("démarrage thread 1");
 		t1.start();
 		System.out.println("démarrage thread 2");
 		t2.start();
-		System.out.println("démarrage worker C dans main");
-		w3.run();
+		System.out.println("démarrage thread 3");
+		t3.start();
 		
+		
+		try {
+			while (true) {
+				System.out.println("attente de thread 1");
+				t1.join(1000);
+				if (!t1.isAlive())
+					break;
+			}
+			System.out.println("attente de thread 2");
+			t2.join();
+			System.out.println("attente de thread 3");
+			t3.join();
+					
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		System.out.println("----------fini----------------");
 		
--- formationdesignPattern/TestThread1/src/com/loncoto/testthread1/util/Worker1.java
+++ formationdesignPattern/TestThread1/src/com/loncoto/testthread1/util/Worker1.java
@@ -13,10 +13,10 @@ public class Worker1 implements Runnable
 	@Override
 	public void run() {
 		double d = 0.0;
-		for (long l = 0; l < 10000000; l++) {
+		for (long l = 0; l < 1000000000; l++) {
 			d =  Math.sqrt(1.0 + l);
 			d = d * 2.0;
-			if (l % 100000 == 0)
+			if (l % 10000000 == 0)
 				System.out.println(nom + ": l = " + l + " d = " + d);
 		}
 	}
